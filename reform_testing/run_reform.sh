ac8d8fbc57ff818152d0e80b549dc673515c259f
Yuwei Sun <ys4680@nyu.edu>
2024-08-14 10:15:21 -0400

    remove testing files

--- reform_testing/run_reform.sh
+++
@@ -1,7 +0,0 @@
-python3 reform.py \
-  --chrom="NC_001133.9" \
-  --position=1,4,230215 \
-  --in_fasta=in1.fa,in2.fa,in3.fa \
-  --in_gff=in1.gff,in2.gff,in3.gff \
-  --ref_fasta=GCF_000146045.2_R64_genomic.fna \
-  --ref_gff=GCF_000146045.2_R64_genomic.gff
